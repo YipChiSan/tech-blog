2535d5ed359b5824c97bb8b610255a310257209d
YipChiSan <zye39@yahoo.com>
2022-08-25 19:34:48 +1000

    Let homepage display all tech blogs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,11 +15,10 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = blogData.map((project) => project.get({ plain: true }));
-
+    const blogs = blogData.map((project) => project.get({ plain: true }));
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      blogs: blogs, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
