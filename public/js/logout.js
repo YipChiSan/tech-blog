6141fe3aa946450240489a707182f475d1de5020
YipChiSan <zye39@yahoo.com>
2022-08-26 14:43:29 +1000

    Display the edit post form

--- public/js/logout.js
+++ public/js/logout.js
@@ -10,5 +10,72 @@ const logout = async () => {
     alert(response.statusText);
   }
 };
+const editHandler = (event) => {
+    event.preventDefault();
+    const blogEl = document.querySelector('#blog');
+    const title = document.querySelector('#blog-title').textContent.trim();
+    const context = document.querySelector('#blog-context').textContent.trim();
+
+    const newForm = document.createElement("form");
+    newForm.setAttribute("class" , "form new-project-form");
+
+    const formTitleDiv = document.createElement('div');
+    formTitleDiv.setAttribute("class", "form-group");
+
+    const formTitleLabel = document.createElement('label');
+    formTitleLabel.setAttribute('for', 'blog-title-input');
+    formTitleLabel.textContent = "Post title:";
+
+    const formTitleInput = document.createElement('input');
+    formTitleInput.setAttribute("class", "form-input");
+    formTitleInput.setAttribute("type", "text");
+    formTitleInput.setAttribute("id", "blog-title-input");
+    formTitleInput.setAttribute("name", "blog-title-input");
+    formTitleInput.setAttribute("value", title);
+
+    formTitleDiv.appendChild(formTitleLabel);
+    formTitleDiv.appendChild(formTitleInput);
+
+    newForm.appendChild(formTitleDiv);
+
+    const formContextDiv = document.createElement('div');
+    formContextDiv.setAttribute("class", "form-group");
+
+    const formContextLabel = document.createElement('label');
+    formContextLabel.setAttribute('for', 'blog-context-input');
+    formContextLabel.textContent = "Post Context:";
+
+    const formContextInput = document.createElement('input');
+    formContextInput.setAttribute("class", "form-input");
+    formContextInput.setAttribute("type", "text");
+    formContextInput.setAttribute("id", "blog-context-input");
+    formContextInput.setAttribute("name", "blog-context-input");
+    formContextInput.setAttribute("value", context);
+
+    formTitleDiv.appendChild(formContextLabel);
+    formTitleDiv.appendChild(formContextInput);
+
+    newForm.appendChild(formContextDiv);
+
+    const formUpdateDiv = document.createElement('div');
+    formUpdateDiv.setAttribute("class", "form-group");
+
+    const formUpdateBtn = document.createElement('button');
+    formUpdateBtn.setAttribute("type", "submit");
+    formUpdateBtn.setAttribute("class", "btn btn-primary");
+    formUpdateBtn.textContent = "Update";
+
+    formUpdateDiv.appendChild(formUpdateBtn);
+    newForm.appendChild(formUpdateDiv);
+
+    blogEl.innerHTML = "";
+    blogEl.appendChild(newForm);
+
+    
+};
+
+document
+  .querySelector('#edit-button')
+  .addEventListener('click', editHandler);
 
 document.querySelector('#logout').addEventListener('click', logout);
